94d4cbf0ab166b1de7c728aec4e96e407eea4111
AVK239 <suka2283310@mail.ru>
2024-03-27 20:36:47 +0700

    Backend

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, Response
 import cv2
 import torch
-import time
+from datetime import datetime, timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people_count.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# Загрузка модели YOLO
+# Определение модели после инициализации db
+class PeopleCount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
+
+# Инициализация и загрузка модели YOLO
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 model.conf = 0.15  # порог доверия
 model.iou = 0.45  # порог IOU
@@ -13,16 +23,28 @@ model.iou = 0.45  # порог IOU
 video_path = 0  # Используйте 0 для веб-камеры
 cap = cv2.VideoCapture(video_path)
 
+# Инициализация переменных для подсчета
+last_hour = datetime.now()
+hourly_count = 0
+
 def generate_frames():
+    global last_hour, hourly_count
     while True:
         success, frame = cap.read()
         if not success:
             break
         else:
-            # Обработка кадра моделью YOLO
             results = model(frame)
+            num_people = sum([1 for det in results.xyxy[0] if int(det[5]) == 0])
+            hourly_count += num_people
 
-            # Отрисовка рамок вокруг обнаруженных объектов
+            if datetime.now() - last_hour >= timedelta(hours=1):
+                new_count = PeopleCount(count=hourly_count)
+                db.session.add(new_count)
+                db.session.commit()
+                last_hour = datetime.now()
+                hourly_count = 0
+
             for *xyxy, conf, cls in results.xyxy[0]:
                 if cls == 0:  # класс 'человек'
                     label = f"{model.names[int(cls)]} {conf:.2f}"
@@ -44,4 +66,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
